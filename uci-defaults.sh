6341190f386c7ecc3ffe5326c9d905763a52c797
reimuc <160387885+reimuc@users.noreply.github.com>
2026-02-05 18:39:08 +0800

    refactor: rename run.yml to tests.yml and update configuration scripts

--- uci-defaults.sh
+++ uci-defaults.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-### === PPPoE 拨号 ===
-uci set network.wan.proto='pppoe'
-uci set network.wan.username='你的宽带账号'
-uci set network.wan.password='你的宽带密码'
-uci commit network
+WAN_USER='宽带账号'
+WAN_PASS='宽带密码'
+WIFI_KEY='WiFi密码'
 
-### === 启用 5GHz WiFi ===
-uci set wireless.radio1.disabled='0'
-uci set wireless.default_radio1.encryption='psk2'
-uci set wireless.default_radio1.key='你的WiFi密码'
-uci commit wireless
+echo "[init] configure PPPoE..."
+uci batch <<EOF
+set network.wan.proto='pppoe'
+set network.wan.username='${WAN_USER}'
+set network.wan.password='${WAN_PASS}'
+commit network
+EOF
 
-### === 重启网络 ===
+echo "[init] configure 5GHz WiFi..."
+uci batch <<EOF
+set wireless.radio1.disabled='0'
+set wireless.default_radio1.encryption='psk2'
+set wireless.default_radio1.key='${WIFI_KEY}'
+commit wireless
+EOF
+
+echo "[init] restart network & wifi..."
 /etc/init.d/network restart
-wifi reload
+wifi reload || wifi
 
+echo "[init] done."
 exit 0
